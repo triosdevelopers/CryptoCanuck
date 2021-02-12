67a0eb600f1e31589384f9e857fed0a273f78694
Allaura Hanley <allauradev@gmail.com>
2021-02-12 13:19:43 -0500

    removed emailblast class
    moved the functions from emailblast class to admin as it is tidier

--- Pages/Admin.cshtml.cs
+++ Pages/Admin.cshtml.cs
@@ -10,6 +10,14 @@ namespace CryptoCanuck.Pages
 {
     public class AdminModel : PageModel
     {
+        public List<string> emails = new List<string>();
+        MailMessage mailMessage = new MailMessage();
+        SmtpClient _smtp = new SmtpClient();
+        string teamsEmail;
+        string teamsEmailPassword;
+        string teamsEmailHost;
+        int teamsEmailPort;
+
         public void OnGet() {} // OnGet()
     
         public void MessageUpdate(string message){
@@ -31,6 +39,77 @@ namespace CryptoCanuck.Pages
                 addMessage.Parameters.Clear(); // clear it
             } // using
             
-        } // SendItOut()
+        } // MessageUpdate()
+
+        public  List<string> GrabEmails(){
+            SqlConnection myConn = new SqlConnection(Program.da.cs);
+            myConn.Open();
+
+            string query = "SELECT email FROM MailerList WHERE status = 'A'";
+            SqlCommand results = new SqlCommand(query, myConn);
+
+            using (SqlDataReader reader = results.ExecuteReader()){
+                while (reader.Read())
+                {
+                    emails.Add(reader.GetString(0));
+                }
+            }// using
+
+            myConn.Close();
+            return emails;
+        } // GrabEmails
+
+        public void SendEmail(object sender, EventArgs e){
+            //get the Settings from WEB.CONFIG file.
+
+            teamsEmail = ConfigurationManager.AppSettings["emailsender"].ToString();
+            teamsEmailPassword = ConfigurationManager.AppSettings["password"].ToString();
+            teamsEmailHost = ConfigurationManager.AppSettings["smtpserver"].ToString();
+            teamsEmailPort = Convert.ToInt16(ConfigurationManager.AppSettings["portnumber"]);
+
+            Boolean emailIsSSL = Convert.ToBoolean(ConfigurationManager.AppSettings["IsSSL"]);
+
+
+            //Getting Email Body Text for Newsletter path will need to be updated for whoever is using at this time.
+            string FilePath = "C:\\Users\\allie\\Documents\\GitHub\\CryptoCanuck\\Pages\\NewsLetter.cshtml.cs";
+            StreamReader str = new StreamReader(FilePath);
+            string MailNews = str.ReadToEnd();
+            str.Close();
+
+            string subject = "New Newsletter Available!";
+
+            GrabEmails();
+
+            for(int i = 0; i < emails.Count; i++){
+
+                // email to 
+                mailMessage.To.Add(emails[i]);
+
+                // body text IS html so true
+                mailMessage.IsBodyHtml = true;
+
+                // the teams email, email subject, email body
+                mailMessage.From = new MailAddress(teamsEmail);
+                mailMessage.Subject = subject;
+                mailMessage.Body = MailNews;
+
+                // Set SMTP, HOST server SMTP detail and PORT
+                SmtpClient _smtp = new SmtpClient();
+                _smtp.Host = teamsEmailHost;
+                _smtp.Port = teamsEmailPort;
+
+                // SSL --> True / False
+                // Secure Sockets Layer encryption between server and client
+                _smtp.EnableSsl = emailIsSSL;
+
+                // Set Sender UserEmailID, Password
+                NetworkCredential _network = new NetworkCredential(teamsEmail, teamsEmailPassword);
+                _smtp.Credentials = _network;
+
+                // Send the email we created
+                _smtp.Send(mailMessage);
+            }
+
+        } // SendEmail
     }
 }
